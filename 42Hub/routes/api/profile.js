a7bafbddd39afd4b6b500981473528b9938a4ac8
Allen Temunovic <allentemunovic@e1z4r7p8.42.us.org>
2019-06-01 21:46:49 -0700

    added github api endpoint for user to add their repos

--- 42Hub/routes/api/profile.js
+++ 42Hub/routes/api/profile.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const request = require('request');
+const config = require('config');
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator/check');
 
@@ -319,6 +321,29 @@ router.delete('/education/:exp_id', auth, async (req, res) => {
     }
 });
 
+// @route      GET api/profile/github/:username
+// @desc       Get user repos from GitHub
+// @access     Public
+router.get('/github/:username', async (req, res) => {
+    try {
+        const options = {
+            uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created: asc&client_id=${config.get('githubClientId')}&client_secret=${config.get('githubSecret')}`,
+            method: 'GET',
+            headers: { 'user-agent': 'node-js' }
+        };
 
+        request(options, (error, response, body) => {
+            if (error) console.error(error);
+
+            if (response.statusCode !== 200) {
+                return res.status(404).json({ msg: 'No GitHub profile found!' });
+            }
+            res.json(JSON.parse(body));
+        });
+    } catch(error) {
+        console.error(error.message);
+        res.status(500).send('Server Error!');
+    }
+})
 
 module.exports = router;
